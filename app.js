52bd74de5af1f9bc14bee610fa12472b307239a9
Shaikh Muhammad Khizar <78459617+Smkhizar7@users.noreply.github.com>
2021-07-09 20:22:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var i = 9;
 var load = () => {
     var content = `<div class="card" style="width: 18rem;"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-<div class="card-body"><h5 class="card-title">Card title</h5>
+<div class="card-body"><h5 class="card-title">Card ${i}</h5>
 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div><div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+1}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div><div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+2}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div><div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+3}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
@@ -30,7 +30,7 @@ var load = () => {
           <div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+4}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
@@ -38,37 +38,55 @@ var load = () => {
           <div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+5}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div><div class="card" style="width: 18rem;">
             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Card ${i+6}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-          </div><div class="card" style="width: 18rem;"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>`;
+          </div><div class="card" style="width: 18rem;"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card ${i+7}</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>`;
     var div = document.getElementById('content');
     div.innerHTML += content;
 }
-var Submit = () => {
-    var email = document.getElementById('inputEmail3').value;
-    var pass = document.getElementById('inputPassword3');
-    if(email.indexOf(" ") !== -1){
+function valid(){
+    var email = document.getElementById('email').value;
+    var pass = document.getElementById('pass');
+    if(email === "" || email === null){
+      alert("please enter email");
+      return false;
+    }
+    else if(email.indexOf(" ") !== -1){
         alert("Email doesn't contain spaces!");
+        return false;
     }
-    if(email.lastIndexOf("@") > (email.length-4)){
+    else if(email.lastIndexOf("@") > (email.length-4)){
         alert("@ character should be placed before last four characters");
+        return false;
     }
-    if(email.lastIndexOf("@") > email.lastIndexOf(".")-2){
+    else if(email.lastIndexOf("@") > email.lastIndexOf(".")-2){
         alert("One character is placed between @ and . character");
+        return false;
     }
-    if(email.lastIndexOf(".") > (email.length-2)){
+    else if(email.lastIndexOf(".") > (email.length-2)){
         alert(". character should be placed before last two characters");
+        return false;
     }
-    if(pass.value.length<8){
+    else if(pass.value === "" || pass.value === null){
+        alert("please enter password");
+        return false;
+    }
+    else if(pass.value.length<8){
         alert("please enter 8 digits password");
+        return false;
     }
+}
+var add = (a) => {
+  var win1 = window.open();
+  var source = document.getElementById(a);
+  win1.document.write(source.innerHTML);
 }
