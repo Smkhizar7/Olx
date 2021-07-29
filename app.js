8b2c2575038e0b8e94f4e8cd74433bc9e129c7b6
Shaikh Muhammad Khizar <78459617+Smkhizar7@users.noreply.github.com>
2021-07-29 15:25:17 +0500

    firebase integration

--- app.js
+++ app.js
@@ -1,104 +1,107 @@
 var i = 9;
 var load = () => {
-  var content = `<div class="card" style="width: 18rem;" id="card${i}"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-<div class="card-body"><h5 class="card-title">Card ${i}</h5>
-<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-<button onclick="add('card${i}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div><div class="card" style="width: 18rem;" id="card${i + 1}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 1}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 1}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div><div class="card" style="width: 18rem;" id="card${i + 2}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 2}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 2}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div><div class="card" style="width: 18rem;" id="card${i + 3}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 3}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 3}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div>
-          <div class="card" style="width: 18rem;" id="card${i + 4}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 4}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 4}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div>
-          <div class="card" style="width: 18rem;" id="card${i + 5}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 5}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 5}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div><div class="card" style="width: 18rem;" id="card${i + 6}">
-            <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card ${i + 6}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <button onclick="add('card${i + 6}')" class="btn btn-primary">Add to Cart</button>
-            </div>
-          </div><div class="card" style="width: 18rem;" id="card${i + 7}"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
-          <div class="card-body"><h5 class="card-title">Card ${i + 7}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button onclick="add('card${i + 7}')" class="btn btn-primary">Add to Cart</button></div></div>`;
+//   var content = `<div class="card" style="width: 18rem;" id="card${i}"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+// <div class="card-body"><h5 class="card-title">Card ${i}</h5>
+// <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div><div class="card" style="width: 18rem;" id="card${i+1}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+1}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>
+//           <div class="card" style="width: 18rem;" id="card${i+2}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+2}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>
+//           <div class="card" style="width: 18rem;" id="card${i+3}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+3}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>
+//           <div class="card" style="width: 18rem;" id="card${i+4}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+4}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>
+//           <div class="card" style="width: 18rem;" id="card${i+5}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+5}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>
+//           <div class="card" style="width: 18rem;" id="card${i+6}">
+//             <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Card ${i+6}</h5>
+//               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//               <a href="#" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div><div class="card" style="width: 18rem;" id="card${i+7}"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+//           <div class="card-body">
+//           <h5 class="card-title">Card ${i+7}</h5>
+//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//           <a href="#" class="btn btn-primary">Go somewhere</a>
+//           </div>
+//           </div>`;
   var div = document.getElementById('content');
-  div.innerHTML += content;
-  i=i+8;
+  var j = i+8;
+  for(; i<j;i++){
+    var content = `<div class="card" style="width: 18rem;" id="card${i}"><img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+      <div class="card-body"><h5 class="card-title">Card ${i}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`;  
+    div.innerHTML += content;
+  }
+  // i += 8;
 }
-function valid() {
-  var email = document.getElementById('email').value;
-  var pass = document.getElementById('pass');
-  if (email === "" || email === null) {
-    alert("please enter email");
-    return false;
-  }
-  else if (email.indexOf(" ") !== -1) {
-    alert("Email doesn't contain spaces!");
-    return false;
-  }
-  else if (email.indexOf("@") === -1) {
-    alert("Email must have one @ character!");
-    return false;
-  }
-  else if (email.indexOf(" ") !== -1) {
-    alert("Email must have one .(dot) character!");
-    return false;
-  }
-  else if (email.lastIndexOf("@") > (email.length - 4)) {
-    alert("@ character should be placed before last four characters");
-    return false;
-  }
-  else if (email.lastIndexOf("@") > email.lastIndexOf(".") - 2) {
-    alert("One character is placed between @ and . character");
-    return false;
-  }
-  else if (email.lastIndexOf(".") > (email.length - 2)) {
-    alert(". character should be placed before last two characters");
-    return false;
-  }
-  else if (pass.value === "" || pass.value === null) {
-    alert("please enter password");
-    return false;
-  }
-  else if (pass.value.length < 8) {
-    alert("please enter 8 digits password");
-    return false;
-  }
+var addToCart = ()=>{
+
 }
-var add = (a) => {
-  var win1 = window.open();
-  var source = document.getElementById(a);
-  win1.document.write(source.innerHTML);
+var login = () => {
+  window.location.href = "./login.html";
+}
+var signIn = () => {
+  var email = document.getElementById('staticEmail2').value;
+  var password = document.getElementById('inputPassword2').value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((data) => {
+    console.log(data)
+    // ...
+  })
+  .catch((error) => {
+    // var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+  });
+}
+var signUp = () => {
+  var email1 = document.getElementById('staticEmail1').value;
+  var password1 = document.getElementById('inputPassword1').value;
+  firebase.auth().createUserWithEmailAndPassword(email1, password1)
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    // var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+  });
 }
